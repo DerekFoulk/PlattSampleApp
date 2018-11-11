727010b0d8fb6049ec2db12c8c961c2ced5738d0
Derek Foulk <derekfoulk@gmail.com>
2018-11-11 00:29:11 -0800

    Cleaned up controller

--- PlattSampleApp/Controllers/HomeController.cs
+++ PlattSampleApp/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PlattSampleApp.Models;
@@ -47,8 +44,6 @@ namespace PlattSampleApp.Controllers
 
             var model = new PlanetResidentsViewModel(planetName, residents);
 
-            // TODO: Implement this controller action
-
             return View(model);
         }
 
@@ -57,8 +52,6 @@ namespace PlattSampleApp.Controllers
             var vehicles = await _swapiService.GetVehicles();
 
             var model = new VehicleSummaryViewModel(vehicles);
-
-            // TODO: Implement this controller action
 
             return View(model);
         }
