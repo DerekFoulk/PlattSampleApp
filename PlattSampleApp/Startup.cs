1c0be58cbd07f2127f194ab51b91a30127d9e94d
Derek Foulk <derekfoulk@gmail.com>
2018-11-12 00:47:23 -0800

    Configured React.AspNet

--- PlattSampleApp/Startup.cs
+++ PlattSampleApp/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PlattSampleApp.Services;
+using React.AspNet;
 
 namespace PlattSampleApp
 {
@@ -20,6 +22,9 @@ namespace PlattSampleApp
         {
             services.AddSingleton<ISwapiService, SwapiService>();
 
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+
             services.AddMvc();
         }
 
@@ -35,6 +40,26 @@ namespace PlattSampleApp
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                //  .AddScript("~/Scripts/First.jsx")
+                //  .AddScript("~/Scripts/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+            });
 
             app.UseStaticFiles();
 
